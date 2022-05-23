700e15ee23da6060cbace602648bcd366b9a4da1
dordor7 <dormailbox@gmail.com>
2022-05-23 07:30:50 -0700

    Splitting to equal dfs with apirs of digits

--- MNist/mnist pandas.py
+++ MNist/mnist pandas.py
@@ -5,8 +5,7 @@ import shutil
 
 df = pd.read_csv('nums_train.csv')
 
-
-
+"""
 # Task 1 - Seperate CSVs for each digit:
 path = "splittedByDigit"
 if os.path.exists(path):
@@ -20,3 +19,23 @@ for i, digit in enumerate(names):
 
     oneDigitDf.to_csv('splittedByDigit/{}-withLabel.csv'.format(digit), header=False, index=False)
     oneDigitDf.iloc[:,:-1].to_csv('splittedByDigit/{}-withoutLabel.csv'.format(digit), header=False, index=False) 
+"""
+
+# Task 2 - Pairs of 50-50 digits:
+path = "pairsOfDigits"
+if os.path.exists(path):
+    shutil.rmtree(path)
+os.mkdir(path)
+
+names = ['Zero+One','One+Two','Two+Three','Three+Four','Four+Five', \
+        'Five+Six','Six+Seven','Seven+Eight','Eight+Nine']
+
+for i, name in enumerate(names):
+    digit1Df = df[df.iloc[:,-1] == i]
+    digit2Df = df[df.iloc[:,-1] == i+1]
+
+    pairDf = pd.concat([digit1Df.head(3795),digit2Df.head(3795)]) # 3795 is the min. number of rows for a digit
+    pairDf = pairDf.sample(frac=1) # Shuffle the rows
+
+    pairDf.to_csv('pairsOfDigits/{}-withLabel.csv'.format(name), header=False, index=False)
+    pairDf.iloc[:,:-1].to_csv('pairsOfDigits/{}-withoutLabel.csv'.format(name), header=False, index=False) 
