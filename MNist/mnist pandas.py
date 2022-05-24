cb2131d0b49cb33adf8bb4895fd1bdee45392705
dordor7 <dormailbox@gmail.com>
2022-05-24 10:09:21 -0700

    Another option for CSV generation was added

--- MNist/mnist pandas.py
+++ MNist/mnist pandas.py
@@ -19,7 +19,7 @@ for i, digit in enumerate(names):
 
     oneDigitDf.to_csv('splittedByDigit/{}-withLabel.csv'.format(digit), header=False, index=False)
     oneDigitDf.iloc[:,:-1].to_csv('splittedByDigit/{}-withoutLabel.csv'.format(digit), header=False, index=False) 
-"""
+
 
 # Task 2 - Pairs of 50-50 digits:
 path = "pairsOfDigits"
@@ -39,3 +39,33 @@ for i, name in enumerate(names):
 
     pairDf.to_csv('pairsOfDigits/{}-withLabel.csv'.format(name), header=False, index=False)
     pairDf.iloc[:,:-1].to_csv('pairsOfDigits/{}-withoutLabel.csv'.format(name), header=False, index=False) 
+"""
+
+# Task 3 - 90% zeros, 10% others:
+path = "zeros90Others10"
+if os.path.exists(path):
+    shutil.rmtree(path)
+os.mkdir(path)
+
+zerosDf = df[df.iloc[:,-1] == 0]
+othersDf = df[df.iloc[:,-1] != 0]
+
+# Make sure no. of rows in zeros df is divisble by 9:
+while (len(zerosDf) % 9 != 0):
+    zerosDf = zerosDf.iloc[:-1][:]
+
+n = len(zerosDf)
+
+zerosOthersDf = pd.concat([zerosDf,othersDf.head(int(n/9))]) 
+zerosOthersDf = zerosOthersDf.sample(frac=1) # Shuffle the rows
+
+"""
+# Make sure ratio is ok:
+bambi1 = zerosOthersDf[zerosOthersDf.iloc[:,-1] == 0]
+bambi2 = zerosOthersDf[zerosOthersDf.iloc[:,-1] != 0]
+
+print(len(bambi1), len(bambi2))
+"""
+
+zerosOthersDf.to_csv('zeros90Others10/zerosOthersDf-withLabel.csv', header=False, index=False)
+zerosOthersDf.iloc[:,:-1].to_csv('zeros90Others10/zerosOthersDf-withoutLabel.csv', header=False, index=False)
