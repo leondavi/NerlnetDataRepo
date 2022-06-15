ce5b922e079a553f2b6fae934c8caead5ac72b13
David <ddleon@outlook.co.il>
2022-06-15 13:19:30 -0700

    remove spaces

--- MNist/mnist_pandas.py
+++ MNist/mnist_pandas.py
@@ -20,7 +20,7 @@ for i, digit in enumerate(names):
     oneDigitDf.to_csv('splittedByDigit/{}-withLabel.csv'.format(digit), header=False, index=False)
     oneDigitDf.iloc[:,:-1].to_csv('splittedByDigit/{}-withoutLabel.csv'.format(digit), header=False, index=False) 
 
-
+"""
 # Task 2 - Pairs of 50-50 digits:
 path = "pairsOfDigits"
 if os.path.exists(path):
@@ -39,8 +39,8 @@ for i, name in enumerate(names):
 
     pairDf.to_csv('pairsOfDigits/{}-withLabel.csv'.format(name), header=False, index=False)
     pairDf.iloc[:,:-1].to_csv('pairsOfDigits/{}-withoutLabel.csv'.format(name), header=False, index=False) 
-"""
 
+"""
 # Task 3 - 90% zeros, 10% others:
 path = "zeros90Others10"
 if os.path.exists(path):
@@ -59,13 +59,14 @@ n = len(zerosDf)
 zerosOthersDf = pd.concat([zerosDf,othersDf.head(int(n/9))]) 
 zerosOthersDf = zerosOthersDf.sample(frac=1) # Shuffle the rows
 
-"""
+
 # Make sure ratio is ok:
 bambi1 = zerosOthersDf[zerosOthersDf.iloc[:,-1] == 0]
 bambi2 = zerosOthersDf[zerosOthersDf.iloc[:,-1] != 0]
 
 print(len(bambi1), len(bambi2))
-"""
+
 
 zerosOthersDf.to_csv('zeros90Others10/zerosOthersDf-withLabel.csv', header=False, index=False)
 zerosOthersDf.iloc[:,:-1].to_csv('zeros90Others10/zerosOthersDf-withoutLabel.csv', header=False, index=False)
+"""
